ab80eaf12581ca9a87f5036261bfb5e9b5acf938
LqLarry <lqlarry@gmail.com>
2023-07-25 19:55:33 -0700

    Fix symlinks in link shell

--- link-files.sh
+++ link-files.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-# ln -sf ~/.git/.dotfiles/.dwm ~/.dwm
-# ln -sf ~/.git/.dotfiles/.bashrc ~/.bashrc
-# ln -sf ~/.git/.dotfiles/.zshrc ~/.zshrc
-# ln -sf ~/.git/.dotfiles/.aliases ~/.aliases
-# ln -sf ~/.git/.dotfiles/.local/bin ~/.local
-# ln -sf ~/.git/.dotfiles/.config/suckless ~/.config/
-ln -sf ~/.git/.dotfiles/.config/dunst ~/.config/
-ln -sf ~/.git/.dotfiles/.config/kitty ~/.config
-ln -sf ~/.git/.dotfiles/.config/mpd ~/.config
-ln -sf ~/.git/.dotfiles/.config/mpv ~/.config
-ln -sf ~/.git/.dotfiles/.config/ncmpcpp ~/.config
-ln -sf ~/.git/.dotfiles/.config/picom ~/.config
-ln -sf ~/.git/.dotfiles/.config/qtile ~/.config
-ln -sf ~/.git/.dotfiles/.config/rofi ~/.config
-ln -sf ~/.git/.dotfiles/.config/transmission ~/.config
+ln -sf ~/.git/dotfiles/.dwm ~/.dwm
+ln -sf ~/.git/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/.git/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.git/dotfiles/.aliases ~/.aliases
+ln -sf ~/.git/dotfiles/.local/bin ~/.local
+ln -sf ~/.git/dotfiles/.config/suckless ~/.config/
+ln -sf ~/.git/dotfiles/.config/dunst ~/.config/
+ln -sf ~/.git/dotfiles/.config/kitty ~/.config
+ln -sf ~/.git/dotfiles/.config/mpd ~/.config
+ln -sf ~/.git/dotfiles/.config/mpv ~/.config
+ln -sf ~/.git/dotfiles/.config/ncmpcpp ~/.config
+ln -sf ~/.git/dotfiles/.config/picom ~/.config
+ln -sf ~/.git/dotfiles/.config/qtile ~/.config
+ln -sf ~/.git/dotfiles/.config/rofi ~/.config
+ln -sf ~/.git/dotfiles/.config/transmission ~/.config
